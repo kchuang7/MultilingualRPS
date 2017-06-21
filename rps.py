a694e72ffdee28a5abbb12e56869fe70b8f29529
Kevin Chuang <masterx23@hotmail.com>
2017-06-21 05:48:53 -0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,18 +1,13 @@
 from random import randint
 
-
 print('Rock Paper Scissors')
 
 o = ['Win!', 'Loss!', 'Tie!', ' > ', ' < ', ' = ']
-
 rps = ['rock', 'paper', 'scissors']
 
 # 0 = rock, 1 = paper, 2 = scissors
-
 a = int(input('(1) rock, (2) paper, (3) scissors: ')) - 1
-
 b = randint(0, 2)
-
 r = (a - b + 2) % 3
 
 print(rps[a] + o[r + 3] + rps[b] + ". " + o[r])
